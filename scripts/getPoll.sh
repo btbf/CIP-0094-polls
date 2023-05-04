940d7eaec95c1a26368049a85754e0626c888c74
BTBF <b.t.b.f.corp@gmail.com>
2023-05-04 18:29:50 +0400

    build tx file

--- scripts/getPoll.sh
+++ scripts/getPoll.sh
@@ -115,6 +115,69 @@ else
     echo "Warn: TX has no CIP-0094 metadata label. Exiting"
 fi
 
+echo "Txファイルを作成します"
+read -p "[Enter] を押して続けます ..."
+
+cd $NODE_HOME
+currentSlot=$(cardano-cli query tip $NODE_NETWORK | jq -r '.slot')
+echo Current Slot: $currentSlot
+
+cd $NODE_HOME 
+cardano-cli query utxo \
+    --address $(cat payment.addr) \
+    $NODE_NETWORK > fullUtxo.out
+
+tail -n +3 fullUtxo.out | sort -k3 -nr | sed -e '/lovelace + [0-9]/d' > balance.out
+
+cat balance.out
+
+tx_in=""
+total_balance=0
+while read -r utxo; do
+    in_addr=$(awk '{ print $1 }' <<< "${utxo}")
+    idx=$(awk '{ print $2 }' <<< "${utxo}")
+    utxo_balance=$(awk '{ print $3 }' <<< "${utxo}")
+    total_balance=$((${total_balance}+${utxo_balance}))
+    echo TxHash: ${in_addr}#${idx}
+    echo ADA: ${utxo_balance}
+    tx_in="${tx_in} --tx-in ${in_addr}#${idx}"
+done < balance.out
+txcnt=$(cat balance.out | wc -l)
+echo Total ADA balance: ${total_balance}
+echo Number of UTXOs: ${txcnt}
+
+cardano-cli transaction build-raw \
+    ${tx_in} \
+    --tx-out $(cat payment.addr)+${total_balance} \
+    --invalid-hereafter $(( ${currentSlot} + 10000)) \
+    --fee 0 \
+    --metadata-json-file ~/git/spo-poll/${answer_file} \
+    --out-file tx.tmp
+
+fee=$(cardano-cli transaction calculate-min-fee \
+    --tx-body-file tx.tmp \
+    --tx-in-count ${txcnt} \
+    --tx-out-count 1 \
+    $NODE_NETWORK \
+    --witness-count 2 \
+    --byron-witness-count 0 \
+    --protocol-params-file params.json | awk '{ print $1 }')
+echo fee: $fee
+
+txOut=$((${total_balance}-${fee}))
+echo txOut: ${txOut}
+
+cardano-cli transaction build-raw \
+    ${tx_in} \
+    --tx-out $(cat payment.addr)+${txOut} \
+    --invalid-hereafter $(( ${currentSlot} + 10000)) \
+    --fee ${fee} \
+    --json-metadata-detailed-schema \
+    --metadata-json-file ~/git/spo-poll/${answer_file} \
+    --out-file tx.raw
+
+echo "tx.rawをエアギャップにコピーして署名してください"
+
 
 
 
